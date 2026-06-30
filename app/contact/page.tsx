42a91bec09aba2001c8e047f0789beb3491e00d3
ketia <65299938+ketiaaa@users.noreply.github.com>
2026-07-01 03:52:24 +1000

    fix: make contact card frames bigger

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -30,13 +30,13 @@ export default function Contact() {
 		<div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:grid-cols-3 lg:gap-16 max-w-3xl">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:grid-cols-3 lg:gap-16 max-w-5xl">
 					{socials.map((s) => (
 						<Card key={s.label}>
 							<Link
 								href={s.href}
 								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16"
+								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-36 lg:pb-64 md:p-16"
 							>
 								<span
 									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
